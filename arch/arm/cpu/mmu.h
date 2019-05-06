6bece567fca979d9bed27586d0928e7ac74365df
Ahmad Fatoum <a.fatoum@pengutronix.de>
2019-05-06 15:03:59 +0200

    UPSTREAM: ARM: mmu: mark uncached regions as eXecute never on v7
    The ARM Cortex-A Series Programmer's Guide notes[1]:
    > When set, the Execute Never (XN) bit in the translation table entry
    > prevents speculative instruction fetches taking place from desired
    > memory locations and will cause a prefetch abort to occur if execution
    > from the memory location is attempted.
    >
    > Typically device memory regions are marked as execute never to prevent
    > accidental execution from such locations, and to prevent undesirable
    > side-effects which might be caused by speculative instruction fetches.
    
    Heed the advice and mark uncached memory with the XN bit, when the
    CPU is >=v7.
    
    It's possible that there are SoCs that have a section shared between
    device memory and the on-chip RAM hosting the PBL.
    In such a section, every page except for the OCRAM's should be mapped XN,
    but as we know of no SoC with such an OCRAM layout, we ignore this
    possibility for now and let mmu_early_enable map sections only.
    
    [1]: 9.6.3 "Execute Never", Version 4.0
    
    Suggested-by: Lucas Stach <l.stach@pengutronix.de>
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- arch/arm/cpu/mmu.h
+++ arch/arm/cpu/mmu.h
@@ -3,6 +3,7 @@
 
 #include <asm/pgtable.h>
 #include <linux/sizes.h>
+#include <asm/system_info.h>
 
 #define PGDIR_SHIFT	20
 #define PGDIR_SIZE	(1UL << PGDIR_SHIFT)
@@ -60,8 +61,13 @@ create_sections(uint32_t *ttb, unsigned long first,
 
 static inline void create_flat_mapping(uint32_t *ttb)
 {
+	unsigned int flags = PMD_SECT_DEF_UNCACHED;
+
+	if (cpu_architecture() >= CPU_ARCH_ARMv7)
+		flags |= PMD_SECT_XN;
+
 	/* create a flat mapping using 1MiB sections */
-	create_sections(ttb, 0, 0xffffffff, PMD_SECT_DEF_UNCACHED);
+	create_sections(ttb, 0, 0xffffffff, flags);
 }
 
 #endif /* __ARM_MMU_H */
